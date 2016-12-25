a3ca915e19a3c9dc5d334504512b111ba99cb521
Haim Rubinstein <Haim6678@gmail.com>
2016-12-25 13:52:58 +0200

    first try 3

---
+++ cmake-build-debug/CMakeFiles/ex3_tm.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/haim/ClionProjects/ex3_tm/Algorithm.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Algorithm.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Bfs.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Bfs.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Bfs_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Bfs_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Driver.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Driver.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Driver_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Driver_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Logic.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Logic.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Logic_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Logic_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/LuxTaxiFactory.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/LuxTaxiFactory.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/LuxTaxiFactory_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/LuxTaxiFactory_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/LuxuryTaxi.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/LuxuryTaxi.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Management.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Management.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Management_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Management_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Map.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Map.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/MapFactory.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/MapFactory.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Map_Factory_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Map_Factory_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Map_Tesr.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Map_Tesr.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Passenger.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Passenger.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Passenger_test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Passenger_test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Point.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Point.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Point_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Point_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Searchable.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Searchable.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Socket.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Socket.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Square.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Square.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Square_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Square_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/StandardTaxi.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/StandardTaxi.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/StndTaxiFactory.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/StndTaxiFactory.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Taxi.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Taxi.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/TaxiCenter.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/TaxiCenter.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Taxi_Center_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Taxi_Center_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Trip.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Trip.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Trip_Test.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Trip_Test.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/UdpSocekt.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/UdpSocekt.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/Validate_Interface.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/Validate_Interface.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/VirtualTaxiFactory.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/VirtualTaxiFactory.cpp.o"
+  "/home/haim/ClionProjects/ex3_tm/main.cpp" "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/CMakeFiles/ex3_tm.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/googletest-master/googletest/include"
+  "../lib/googletest-master/googlemock/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/home/haim/ClionProjects/ex3_tm/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/haim/ClionProjects/ex3_tm/lib/googletest-master/googletest
+# Build directory: /home/haim/ClionProjects/ex3_tm/cmake-build-debug/lib/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
